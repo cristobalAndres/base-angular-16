8ccb0c36ef4dba8b2753e739307af6dd5e7a0c77
Cristóbal Gajardo Vera <cristobalalexander.gajardovera@externos-cl.cencosud.com>
2023-06-16 14:02:09 -0400

    feat(transactions): add go back button

--- src/app/pages/transactions/transactions.module.ts
+++ src/app/pages/transactions/transactions.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { SharedModule } from '@app/shared';
 import { TransactionsRoutingModule } from './transactions-routing.module';
 import { TransactionsComponent } from './transactions.component';
 
 @NgModule({
   declarations: [TransactionsComponent],
-  imports: [CommonModule, TransactionsRoutingModule],
+  imports: [CommonModule, SharedModule, TransactionsRoutingModule],
 })
 export class TransactionsModule {}
