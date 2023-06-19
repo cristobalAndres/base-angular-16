389443a03c92124e67a0e3fb788dd09d8e74615f
Cristóbal Gajardo Vera <cristobalalexander.gajardovera@externos-cl.cencosud.com>
2023-06-19 01:44:59 -0400

    feat(transactions): integrate api calls to transactions component

--- src/app/pages/transactions/transactions.component.ts
+++ src/app/pages/transactions/transactions.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { TransactionsService } from './transactions-service';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TransactionsComponent {}
+export class TransactionsComponent {
+  private static readonly ITEMS_PER_PAGE = 11;
+
+  private readonly transactionsService = inject(TransactionsService);
+
+  private readonly transactions$ = this.transactionsService.getTransactions({
+    perPage: TransactionsComponent.ITEMS_PER_PAGE,
+  });
+
+  protected readonly transactions = toSignal(
+    this.transactions$.pipe(map(({ transactions }) => transactions)),
+  );
+
+  protected readonly paginationData = toSignal(
+    this.transactions$.pipe(
+      map(({ total_items, total_pages, current_page }) => ({
+        totalItems: total_items,
+        totalPages: total_pages,
+        currentPage: current_page,
+        itemsPerPage: TransactionsComponent.ITEMS_PER_PAGE,
+      })),
+    ),
+  );
+}
--- src/app/pages/transactions/transactions.module.ts
+++ src/app/pages/transactions/transactions.module.ts
@@ -1,14 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
 import { SharedModule } from '@app/shared';
+import { TransactionsFiltersComponent } from './transactions-filters';
 import { TransactionsRoutingModule } from './transactions-routing.module';
 import { TransactionsService } from './transactions-service';
+import { TransactionsTableComponent } from './transactions-table';
 import { TransactionsComponent } from './transactions.component';
 
 @NgModule({
   declarations: [TransactionsComponent],
-  imports: [CommonModule, SharedModule, TransactionsRoutingModule],
   providers: [TransactionsService],
+  imports: [
+    CommonModule,
+    SharedModule,
+    TransactionsRoutingModule,
+    TransactionsFiltersComponent,
+    TransactionsTableComponent,
+  ],
 })
 export class TransactionsModule {}
