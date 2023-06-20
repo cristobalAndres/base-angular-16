0a8f8ff04af8999eb80ffb1f40f4ea5abe1caf28
Cristóbal Gajardo Vera <cristobalalexander.gajardovera@externos-cl.cencosud.com>
2023-06-20 14:19:58 -0400

    feat(transactions): add status filter

--- src/app/pages/transactions/features/transactions-filters/transactions-filters.component.ts
+++ src/app/pages/transactions/features/transactions-filters/transactions-filters.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '@app/shared';
 import { TransactionsFiltersService } from '../../data-access';
+import { TransactionStatus } from '../../shared';
+import { SelectionDto } from '@app/shared/components/forms';
 
 @Component({
   selector: 'app-transactions-filters',
@@ -15,6 +17,25 @@ export class TransactionsFiltersComponent {
     TransactionsFiltersService,
   );
 
+  protected readonly statusOptions: SelectionDto<TransactionStatus>[] = [
+    { value: TransactionStatus.ALL, label: 'Todos' },
+    { value: TransactionStatus.CONFIRMED, label: 'Confirmado' },
+    {
+      value: TransactionStatus.REJECTED_BY_USER,
+      label: 'Rechazado por usuario',
+    },
+    {
+      value: TransactionStatus.INIT_TRANSACTION,
+      label: 'Transacción iniciada',
+    },
+    { value: TransactionStatus.REVERSED, label: 'Reversado' },
+    { value: TransactionStatus.REJECTED, label: 'Rechazado' },
+    {
+      value: TransactionStatus.NOTIFICATION_SUCCESS,
+      label: 'Notificación exitosa',
+    },
+  ];
+
   protected onStartDateChange(date: Date | undefined) {
     this.transactionsFiltersService.updateTransactionsFilters({
       startDate: date,
@@ -24,6 +45,12 @@ export class TransactionsFiltersComponent {
   protected onEndDateChange(date: Date | undefined) {
     this.transactionsFiltersService.updateTransactionsFilters({
       endDate: date,
+    });
+  }
+
+  protected onStatusChange(status: TransactionStatus | undefined) {
+    this.transactionsFiltersService.updateTransactionsFilters({
+      statusFilter: status,
     });
   }
 }
