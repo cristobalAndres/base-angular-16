e32717451098ce2cd14538466a4974e78da4e2bd
Cristóbal Gajardo Vera <cristobalalexander.gajardovera@externos-cl.cencosud.com>
2023-06-20 12:24:33 -0400

    feat(components): create range datepicker component

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -2,9 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NavigateButtonComponent } from './buttons';
 import { PaginationComponent } from './tables';
+import { RangeDatepickerComponent } from './forms';
 
 @NgModule({
-  imports: [CommonModule, NavigateButtonComponent, PaginationComponent],
-  exports: [NavigateButtonComponent, PaginationComponent],
+  imports: [
+    CommonModule,
+    NavigateButtonComponent,
+    PaginationComponent,
+    RangeDatepickerComponent,
+  ],
+  exports: [
+    NavigateButtonComponent,
+    PaginationComponent,
+    RangeDatepickerComponent,
+  ],
 })
 export class ComponentsModule {}
