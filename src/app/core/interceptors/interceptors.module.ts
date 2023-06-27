a3e611e646b1c4edabacbb04e08332dae476b2a6
Cristóbal Gajardo Vera <cristobalalexander.gajardovera@externos-cl.cencosud.com>
2023-06-27 11:52:46 -0400

    feat(auth): add access token when sending requests

--- src/app/core/interceptors/interceptors.module.ts
+++ src/app/core/interceptors/interceptors.module.ts
@@ -1,9 +1,15 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ApiBaseUrlInterceptor } from './api-base-url';
+import { AuthInterceptor } from './auth';
 
 @NgModule({
   providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ApiBaseUrlInterceptor,
