02c02f4275dd0bc4fba2ef21757cb6c0627e7e78
Cristóbal Gajardo Vera <cristobalalexander.gajardovera@externos-cl.cencosud.com>
2023-07-19 16:15:12 -0400

    chore(scripts): add environments-related scripts
    There were added local, develop, qa, and production environments.

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -2,8 +2,7 @@ import { Environment } from './environment.type';
 
 export const environment: Environment = {
   production: false,
-  apiUrl:
-    'https://w5h9f864a1.execute-api.us-east-1.amazonaws.com/qa/cw-cl-users-back-manager/api/v1',
+  apiUrl: 'http://localhost:3002/api/v1',
   apiKYC:
     'https://cw-cl-user-data-develop.cencosud.net/user-data/user/KYC/enable',
 };
