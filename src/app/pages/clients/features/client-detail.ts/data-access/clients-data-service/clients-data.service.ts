425cc76894783cffc480a10df0759103713c9a73
Felipe Caceres Osadey <fcaceres@acid.cl>
2023-08-08 18:20:55 -0400

    feat(client-data.service): add change status client method

--- src/app/pages/clients/features/client-detail.ts/data-access/clients-data-service/clients-data.service.ts
+++ src/app/pages/clients/features/client-detail.ts/data-access/clients-data-service/clients-data.service.ts
@@ -1,11 +1,22 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { ClientsService } from '@app/pages/clients/data-access';
 import { ClientDto } from '@app/pages/clients/shared';
-import { lastValueFrom } from 'rxjs';
+import { ToastService } from '@app/shared/services';
+import { ToastsColors } from '@app/shared/services/toasts';
+import {
+  Observable,
+  catchError,
+  finalize,
+  first,
+  lastValueFrom,
+  tap,
+  throwError,
+} from 'rxjs';
 
 @Injectable()
 export class ClientsDataService {
   private readonly clientsService = inject(ClientsService);
+  private readonly toastService = inject(ToastService);
 
   private isLoadingSig = signal(false);
   private hasErrorSig = signal(false);
@@ -14,6 +25,12 @@ export class ClientsDataService {
   readonly client = computed(() => this.clientSig());
   readonly isLoading = computed(() => this.isLoadingSig());
   readonly hasError = computed(() => this.hasErrorSig());
+
+  private isLoadingChangeStatusSig = signal(false);
+
+  readonly isLoadingChangeStatus = computed(() =>
+    this.isLoadingChangeStatusSig(),
+  );
 
   async loadClient(clientId: string) {
     this.hasErrorSig.set(false);
@@ -29,6 +46,45 @@ export class ClientsDataService {
     } finally {
       this.isLoadingSig.set(false);
     }
+  }
+
+  changeStatusClient() {
+    const blockService$ = this.clientSig().blocked
+      ? this.clientsService.unBlockClient(this.clientSig().id ?? '')
+      : this.clientsService.blockClient(this.clientSig().id ?? '');
+
+    return this.changeStatus(blockService$).subscribe();
+  }
+
+  private changeStatus(observer: Observable<ClientDto>) {
+    this.isLoadingChangeStatusSig.set(true);
+    this.toastService.show({
+      body: 'Enviando solicitud...',
+      color: ToastsColors.PRIMARY,
+    });
+    return observer.pipe(
+      finalize(() => {
+        this.isLoadingChangeStatusSig.set(false);
+      }),
+      first(),
+      catchError((error: unknown) => {
+        this.toastService.clear();
+        this.toastService.show({
+          body: 'No se ha podido actualizar la información',
+          color: ToastsColors.DANGER,
+        });
+
+        return throwError(() => error);
+      }),
+      tap((client) => {
+        this.toastService.clear();
+        this.toastService.show({
+          body: 'Se ha actualizado la información correctamente',
+          color: ToastsColors.SUCCESS,
+        });
+        this.clientSig.set(client);
+      }),
+    );
   }
 
   cleanData() {
