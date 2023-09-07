bcae02a4445bbb9c49b157e82d4f76686fb1aefd
Felipe Caceres Osadey <fcaceres@acid.cl>
2023-09-07 13:27:40 -0300

    feat(auth.interceptor): add token to apiReportUrl

--- src/app/pages/reports/features/transaction-report/data-access/report-service/report.service.ts
+++ src/app/pages/reports/features/transaction-report/data-access/report-service/report.service.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { environment } from '@environment';
-import { Auth } from 'aws-amplify';
-import { from, map, mergeMap } from 'rxjs';
 import { ReportDataDto } from '../../shared/dtos';
 
 @Injectable({
@@ -12,21 +10,9 @@ export class ReportService {
   private readonly httpClient = inject(HttpClient);
 
   generateReport(data: ReportDataDto) {
-    return from(Auth.currentSession()).pipe(
-      map((session) => session.getAccessToken()),
-      map((accessToken) => accessToken.getJwtToken()),
-      map((jwtAccessToken) => jwtAccessToken),
-      mergeMap((jwtAccessToken) => {
-        return this.httpClient.post(
-          `${environment.apiReportUrl}/report/transactions`,
-          data,
-          {
-            headers: {
-              Authorization: `Bearer ${jwtAccessToken}`,
-            },
-          },
-        );
-      }),
+    return this.httpClient.post(
+      `${environment.apiReportUrl}/report/transactions`,
+      data,
     );
   }
 }
