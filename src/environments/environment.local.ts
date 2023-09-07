1db15d27da4e33a78e3a4086392f743e9a911c0d
Felipe Caceres Osadey <fcaceres@acid.cl>
2023-09-07 13:30:56 -0300

    feat(env.local): restore local env

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -2,9 +2,8 @@ import { Environment } from './environment.type';
 
 export const environment: Environment = {
   production: false,
-  apiUrl:
-    'https://w5h9f864a1.execute-api.us-east-1.amazonaws.com/qa/cw-cl-users-back-manager/api/v1',
-  apiReportUrl: 'api-report-url',
+  apiUrl: 'http://localhost:3002/api/v1',
+  apiReportUrl: 'http://localhost:3000/api/v1',
   apiKYC:
     'https://cw-cl-user-data-develop.cencosud.net/user-data/user/KYC/enable',
   region: 'us-east-1',
