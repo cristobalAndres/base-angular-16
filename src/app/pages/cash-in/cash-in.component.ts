52a51a61062d35490f3f6b4d7aa11858639970e2
Cristóbal Gajardo Vera <cristobalalexander.gajardovera@externos-cl.cencosud.com>
2023-09-21 10:13:26 -0300

    feat(cash-in): add cash-in module

---
+++ src/app/pages/cash-in/cash-in.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cash-in',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cash-in.component.html',
+  styleUrls: ['./cash-in.component.scss'],
+})
+export class CashInComponent {}
---
+++ src/app/pages/cash-in/cash-in.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CashInRoutingModule } from './cash-in-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, CashInRoutingModule],
+})
+export class CashInModule {}
