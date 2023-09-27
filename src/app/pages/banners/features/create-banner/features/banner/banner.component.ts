4d923e175f00f8e0999d3d747c62ea7a7bee9e14
Felipe Caceres Osadey <fcaceres@acid.cl>
2023-09-26 21:02:03 -0300

    feat(banner.component): add update banner

--- src/app/pages/banners/features/create-banner/features/banner/banner.component.ts
+++ src/app/pages/banners/features/create-banner/features/banner/banner.component.ts
@@ -2,24 +2,29 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  Input,
   OnInit,
   inject,
   signal,
 } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BannersService } from '@app/pages/banners/data-access/banners-service';
+import { BannerDto } from '@app/pages/banners/shared/dtos/banner-dto';
 import {
   ActionType,
   CreateBannerDto,
   PromotionType,
+  UpdateBannerRequestDto,
 } from '@app/pages/clients/shared';
 import { ReactiveFormInputTextComponent } from '@app/shared/components/reactive-form';
 import { ToastService } from '@app/shared/services';
+import { ConfirmModalService } from '@app/shared/services/modals/confirm-modal/confirm-modal.service';
+import { ConfirmModalResponse } from '@app/shared/services/modals/confirm-modal/enums/confirm-moda-respnse.enum';
 import { ToastsColors } from '@app/shared/services/toasts';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 import { ListItem } from 'ng-multiselect-dropdown/multiselect.model';
-import { catchError, first, mergeMap, tap } from 'rxjs';
+import { catchError, first, mergeMap, pipe, tap } from 'rxjs';
 import { Commerces } from '../../dtos/commerces.enum';
 
 @Component({
@@ -36,16 +41,20 @@ import { Commerces } from '../../dtos/commerces.enum';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BannerComponent implements OnInit {
+  @Input() promotionToEdit: BannerDto | undefined;
+
   private readonly fb = inject(FormBuilder);
   private readonly bannerService = inject(BannersService);
   private readonly toastService = inject(ToastService);
   private readonly modalService = inject(NgbModal);
+  private readonly confirmModalService = inject(ConfirmModalService);
 
   isLoading = signal(false);
 
   URL = URL;
 
   previewImage: File | null = null;
+  imageBannerUrl = signal<string | undefined>(undefined);
 
   action_types = [
     { id: ActionType.WEBVIEW, name: 'Website' },
@@ -53,7 +62,6 @@ export class BannerComponent implements OnInit {
   ];
 
   dropdownList: ListItem[] = [];
-  selectedItems: ListItem[] = [];
   dropdownSettings = {};
 
   bannerForm = this.fb.group({
@@ -72,10 +80,42 @@ export class BannerComponent implements OnInit {
     from_date: ['', Validators.required],
     to_date: ['', [Validators.required]],
     country: ['CL'],
+    commerces: [[] as ListItem[], [Validators.required]],
   });
 
   ngOnInit() {
     this.setSettingsDropDownStores();
+
+    if (this.promotionToEdit) {
+      this.bannerForm.setValue({
+        id_promotion: this.promotionToEdit.id_promotion,
+        title_text: this.promotionToEdit.title_text,
+        off_text: this.promotionToEdit.off_text ?? '',
+        badge_text: this.promotionToEdit.badge_text,
+        badge_background_color: this.promotionToEdit.badge_background_color,
+        image_banner_url: this.promotionToEdit.image_banner_url ?? '',
+        action_type: this.promotionToEdit.action_type,
+        action_type_url: this.promotionToEdit.action_type_url ?? '',
+        type_promotion: this.promotionToEdit.type_promotion,
+        header_text: this.promotionToEdit.header_text ?? '',
+        subtitle_text: this.promotionToEdit.subtitle_text ?? '',
+        active: this.promotionToEdit.active,
+        from_date: new Date(this.promotionToEdit.from_date)
+          .toISOString()
+          .split('T')[0],
+        to_date: new Date(this.promotionToEdit.to_date)
+          .toISOString()
+          .split('T')[0],
+        country: this.promotionToEdit.country,
+        commerces: this.dropdownList.filter((i) => {
+          return Array.from(
+            this.promotionToEdit?.filter_attributes ?? [],
+          ).includes(i.id.toString());
+        }),
+      });
+
+      this.imageBannerUrl.set(this.promotionToEdit.image_banner_url);
+    }
   }
 
   setSettingsDropDownStores() {
@@ -106,26 +146,21 @@ export class BannerComponent implements OnInit {
     }
     this.previewImage = img;
   }
-  onItemSelect(item: ListItem) {
-    this.selectedItems.push(item);
-  }
-  onSelectAll(items: ListItem[]) {
-    this.selectedItems = items;
-  }
-  onItemDeSelect(item: ListItem) {
-    this.selectedItems = this.selectedItems.filter((i) => i.id !== item.id);
-  }
-  onDeSelectAll() {
-    this.selectedItems = [];
-  }
 
-  saveForm() {
-    if (
-      this.bannerForm.invalid ||
-      this.selectedItems.length === 0 ||
-      !this.previewImage
-    ) {
+  async saveForm() {
+    if (this.bannerForm.invalid || !this.previewImage) {
       throw new Error('Formulario invalido');
+    }
+
+    const result = await this.confirmModalService.open({
+      title: 'Confirmar',
+      message: '¿Está seguro que desea crear el banner?',
+      primaryButtonText: 'Confirmar',
+      secondaryButtonText: 'Cancelar',
+    });
+
+    if (result === ConfirmModalResponse.SECONDARY_BUTTON_CLICKED) {
+      return;
     }
 
     this.isLoading.set(true);
@@ -142,11 +177,18 @@ export class BannerComponent implements OnInit {
       .uploadBannerImage(formData)
       .pipe(
         mergeMap(({ image_url }) => {
+          const commerces = this.bannerForm.get('commerces')?.value ?? [];
+
+          const formValue = { ...this.bannerForm.value };
+
+          delete formValue.commerces;
+
           const body = {
-            ...this.bannerForm.value,
+            ...formValue,
             image_banner_url: image_url,
-            filter_attributes: this.selectedItems.map((i) => i.id.toString()),
+            filter_attributes: commerces?.map((i) => i.id.toString()),
           };
+
           return this.bannerService.createBanner(body as CreateBannerDto);
         }),
         catchError(() => {
@@ -172,5 +214,98 @@ export class BannerComponent implements OnInit {
         first(),
       )
       .subscribe();
+  }
+
+  async updateForm() {
+    if (this.bannerForm.invalid) {
+      throw new Error('Formulario invalido');
+    }
+
+    const result = await this.confirmModalService.open({
+      title: 'Confirmar',
+      message: '¿Está seguro que desea actualizar el banner?',
+      primaryButtonText: 'Confirmar',
+      secondaryButtonText: 'Cancelar',
+    });
+
+    if (result === ConfirmModalResponse.SECONDARY_BUTTON_CLICKED) {
+      return;
+    }
+
+    this.isLoading.set(true);
+    this.toastService.show({
+      body: 'Cargando...',
+      color: ToastsColors.PRIMARY,
+      delay: 20000,
+    });
+
+    if (this.previewImage) {
+      return this.updateBannerWithImage().subscribe();
+    }
+
+    return this.updateBanner(undefined)
+      .pipe(this.genericPipeToUpdate())
+      .subscribe();
+  }
+
+  updateBannerWithImage() {
+    if (!this.previewImage) {
+      throw new Error('Image is required');
+    }
+
+    const formData = new FormData();
+    formData.append('file', this.previewImage);
+
+    return this.bannerService.uploadBannerImage(formData).pipe(
+      mergeMap(({ image_url }) => {
+        return this.updateBanner(image_url);
+      }),
+      this.genericPipeToUpdate(),
+    );
+  }
+
+  updateBanner(image_url: string | undefined) {
+    const commerces = this.bannerForm.get('commerces')?.value ?? [];
+
+    const formValue = { ...this.bannerForm.value };
+
+    delete formValue.commerces;
+
+    const body = {
+      ...formValue,
+      image_banner_url: image_url ?? this.promotionToEdit?.image_banner_url,
+      filter_attributes: commerces?.map((i) => i.id.toString()),
+    };
+
+    return this.bannerService.updateBannerById(
+      this.promotionToEdit?.id ?? '',
+      body as UpdateBannerRequestDto,
+    );
+  }
+
+  genericPipeToUpdate() {
+    return pipe(
+      catchError(() => {
+        this.isLoading.set(false);
+        this.toastService.clear();
+        this.toastService.show({
+          body: 'Error al actualizar banner',
+          color: ToastsColors.DANGER,
+          delay: 5000,
+        });
+        throw Error('Error al actualizar banner');
+      }),
+      tap(() => this.isLoading.set(false)),
+      tap(() => this.toastService.clear()),
+      tap(() =>
+        this.toastService.show({
+          body: 'Banner actualizado correctamente',
+          color: ToastsColors.SUCCESS,
+          delay: 2000,
+        }),
+      ),
+      tap(() => this.modalService.dismissAll()),
+      first(),
+    );
   }
 }
