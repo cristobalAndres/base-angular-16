2f29d96ef64b0001ea8dea2c995a2dffcc260ce3
Felipe Caceres Osadey <fcaceres@acid.cl>
2023-09-26 21:01:33 -0300

    feat(crate-banner.component): add update banner

--- src/app/pages/banners/features/create-banner/create-banner.component.ts
+++ src/app/pages/banners/features/create-banner/create-banner.component.ts
@@ -1,12 +1,14 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  Input,
   inject,
   signal,
 } from '@angular/core';
 import { PromotionType } from '@app/pages/clients/shared';
 import { SelectionDto } from '@app/shared/components/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BannerDto } from '../../shared/dtos/banner-dto';
 
 @Component({
   selector: 'app-create-banner',
@@ -14,7 +16,21 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateBannerComponent {
+  @Input() promotionToEdit: BannerDto | undefined;
+
   private readonly modalService = inject(NgbModal);
+
+  get TypePromotion(): string {
+    if (!this.promotionToEdit && this.options) return '';
+
+    return (
+      (this.options ?? [])
+        .filter(
+          (option) => option.value === this.promotionToEdit?.type_promotion,
+        )
+        .at(0)?.label ?? ''
+    );
+  }
 
   PROMOTION_OPTIONS = PromotionType;
   promotionTypeSelected = signal<PromotionType | undefined>(undefined);
