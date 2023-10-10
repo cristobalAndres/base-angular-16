e7a44a44dd4264d3aab56ba282dedc76790224fc
Felipe Caceres Osadey <fcaceres@acid.cl>
2023-10-10 19:47:24 -0300

    feat(env.master): add api report url

--- src/environments/environment.master.ts
+++ src/environments/environment.master.ts
@@ -4,7 +4,8 @@ export const environment: Environment = {
   production: true,
   apiUrl:
     'https://brxfzqtdae.execute-api.us-east-1.amazonaws.com/prod/cw-cl-users-back-manager/api/v1',
-  apiReportUrl: '',
+  apiReportUrl:
+    'https://bh8xeay84j.execute-api.us-east-1.amazonaws.com/prod/cw-cl-report-manager/api/v1',
   apiKYC: 'https://cw-cl-user-data-prod.cencosud.net/user-data/user/KYC/enable',
   region: 'us-east-1',
   userPoolId: 'us-east-1_p7wcOS7Rp',
